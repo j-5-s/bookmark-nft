c264c5d02bb9a13e124f2f658b8b947f1febc379
James Charlesworth <james.charlesworth@gmail.com>
2023-06-09 08:27:28 -0400

    fixes tests

--- apps/nft-j5s-dev/specs/index.spec.tsx
+++ apps/nft-j5s-dev/specs/index.spec.tsx
@@ -1,8 +1,28 @@
 import React from "react";
-import { render } from "@testing-library/react";
 
+import { render } from "@testing-library/react";
 import Index from "../pages/index";
 
+jest.mock("next/router", () => ({
+  useRouter() {
+    return {
+      route: "/",
+      pathname: "",
+      query: "",
+      asPath: "",
+    };
+  },
+}));
+
+jest.mock("dexie-react-hooks", () => ({
+  useLiveQuery: jest.fn(() => []),
+}));
+jest.mock("@rainbow-me/rainbowkit", () => ({
+  ConnectButton: jest.fn(),
+  RainbowKitProvider: jest.fn(),
+  getDefaultWallets: jest.fn(() => ({})),
+}));
+// @todo tests break because rainbowkit jest issues
 describe("Index", () => {
   it("should render successfully", () => {
     const { baseElement } = render(<Index />);
