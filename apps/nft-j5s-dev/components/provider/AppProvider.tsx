d0b30b44eec534077499e586ad0a874ff5ca5cf3
James Charlesworth <james.charlesworth@gmail.com>
2023-06-14 17:00:26 -0400

    adds metatags

--- apps/nft-j5s-dev/components/provider/AppProvider.tsx
+++ apps/nft-j5s-dev/components/provider/AppProvider.tsx
@@ -1,4 +1,5 @@
 import React, { ReactNode } from "react";
+import Head from "next/head";
 interface IAppProvider {
   children: ReactNode;
   // Other props...
@@ -14,5 +15,16 @@ export const AppProvider: React.FC<IAppProvider> = ({ children }) => {
   //   })();
   // }, []);
 
-  return <>{children}</>;
+  return (
+    <>
+      <Head>
+        <meta
+          name="content-nft-address"
+          content="0x7bb8bedd4a1106cf90cff35f8b11ea6a3d0661e9"
+        />
+        <meta name="nft_contract_network" content="maticmum" />
+      </Head>
+      {children}
+    </>
+  );
 };
